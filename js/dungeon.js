ed99d1cc713715cc4f477716d846b06e88cd10fa
Jorg Nieberg <jnieberg@gmail.com>
2016-05-13 14:48:02 +0200

    Debug

--- js/dungeon.js
+++ js/dungeon.js
@@ -1,4 +1,4 @@
-var debug = true;
+var debug = false;
 var stereo = false;
 var dir = [{x: 0, y: -1}, {x: 1, y: 0}, {x: 0, y: 1}, {x: -1, y: 0}];
 if(comp.device.toLowerCase() === '') {
@@ -1253,46 +1253,44 @@ function printDebug() {
 	}
 }
 
-if(debug) {
-	(function () {
-	    "use strict";
+(function () {
+    "use strict";
 
-	    var methods, generateNewMethod, i, j, cur, old, addEvent;
+    var methods, generateNewMethod, i, j, cur, old, addEvent;
 
-	    if ("console" in window) {
-	        methods = [
-	            "log", "assert", "clear", "count",
-	            "debug", "dir", "dirxml", "error",
-	            "exception", "group", "groupCollapsed",
-	            "groupEnd", "info", "profile", "profileEnd",
-	            "table", "time", "timeEnd", "timeStamp",
-	            "trace", "warn"
-	        ];
+    if ("console" in window) {
+        methods = [
+            "log", "assert", "clear", "count",
+            "debug", "dir", "dirxml", "error",
+            "exception", "group", "groupCollapsed",
+            "groupEnd", "info", "profile", "profileEnd",
+            "table", "time", "timeEnd", "timeStamp",
+            "trace", "warn"
+        ];
 
-	        generateNewMethod = function (oldCallback, methodName) {
-	            return function () {
-	                var args;
-	                //alert("called console." + methodName + ", with " + arguments.length + " argument(s)");
-	                args = Array.prototype.slice.call(arguments, 0);
-	                Function.prototype.apply.call(oldCallback, console, arguments);
-	            };
-	        };
+        generateNewMethod = function (oldCallback, methodName) {
+            return function () {
+                var args;
+                //alert("called console." + methodName + ", with " + arguments.length + " argument(s)");
+                args = Array.prototype.slice.call(arguments, 0);
+                Function.prototype.apply.call(oldCallback, console, arguments);
+            };
+        };
 
-	        for (i = 0, j = methods.length; i < j; i++) {
-	            cur = methods[i];
-	            if (cur in console) {
-	                old = console[cur];
-	                console[cur] = generateNewMethod(old, cur);
-	            }
-	        }
-	    }
+        for (i = 0, j = methods.length; i < j; i++) {
+            cur = methods[i];
+            if (cur in console) {
+                old = console[cur];
+                console[cur] = generateNewMethod(old, cur);
+            }
+        }
+    }
 
-	    window.onerror = function (msg, url, line) {
-	    	if($('body > #log').length === 0) {
-	    		$('body').append('<div id="log"></div>')
-	    	}
-	        $('#log').text($('#log').html() + msg + ", " + url + ", line " + line);
-	        $('#log').append('<br>');
-	    };
-	}());
-}
+    window.onerror = function (msg, url, line) {
+    	if($('body > #log').length === 0) {
+    		$('body').append('<div id="log"></div>')
+    	}
+        $('#log').text($('#log').html() + msg + ", " + url + ", line " + line);
+        $('#log').append('<br>');
+    };
+}());
